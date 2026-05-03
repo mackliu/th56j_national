0fff607ef18831c9dc44e09e915ee59d82f02c46
macklun <macklun@gmail.com>
2026-05-03 10:53:41 +0800

    載入所需外部套件,並完成主頁切版

---
+++ admin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>56屆選手練習</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+</head>
+<body>
+  <header  class="d-flex p-4 justify-content-between border-bottom">
+    <div>
+        <img src="icon/logo.png" alt="">
+        <a href="index.php">大眾運輸查詢系統</a>
+    </div>
+    <div>
+        <a class='mx-3' href="admin.php">系統管理</a>
+        <a class='mx-3' href="logout.php">登出</a>
+    </div>
+  </header>
+  <main>
+
+  </main>
+  <footer>
+
+  </footer>
+   
+   
+<script src="js/jquery-3.7.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
+<script>
+
+
+
+</script>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>56屆選手練習</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+</head>
+<body class='container'>
+  <header class="d-flex p-4 justify-content-between border-bottom">
+    <div>
+        <img src="icon/logo.png" alt="">
+        <a href="index.php">大眾運輸查詢系統</a>
+    </div>
+    <div>
+        <a href="admin.php">系統管理</a>
+    </div>
+  </header>
+  <main class='p-3'>
+    <div class="form-group">
+        <select name="route" id="" class="d-block m-auto col-6 form-control">
+            <option value="1">路線1</option>
+            <option value="2">路線2</option>
+            <option value="3">路線3</option>
+            <option value="4">路線4</option>
+            <option value="5">路線5</option>
+        </select>
+        
+    </div>
+    <div class="w-full" id="RouteMap">路網圖</div>
+
+  </main>
+  <footer></footer>
+   
+   
+<script src="js/jquery-3.7.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
+<script>
+
+
+
+</script>
