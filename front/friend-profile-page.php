c09f2bfa82c2d7ac3998e122737993d541b20789
macklun <macklun@gmail.com>
2026-05-10 11:45:10 +0800

    更正標籤錯誤(div -> article)

--- front/friend-profile-page.php
+++ front/friend-profile-page.php
@@ -8,12 +8,12 @@
 
 <div class="profile-content">
     <section class="articles">
-        <div class="article-item">
+        <article class="article-item">
             <div class="article-title"></div>
             <time datetime="" class="article-date"></time>
             <div class="article-excerpt"></div>
             <a href="" class="article-readmore">閱讀更多</a>
-        </div>
+        </article>
         <div class="empty-article-message">目前尚無文章</div>
     </section>    
 </div>
