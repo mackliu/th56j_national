e5b4b974ba66552c2256c3406f3c619ef91d851f
macklun <macklun@gmail.com>
2026-05-10 11:39:27 +0800

    增加遊戲頁面版型標籤及CLASS

---
+++ front/game-play.php
@@ -0,0 +1,17 @@
+<div id="game-play">
+    <div class="current-game-title"></div>
+    <section class="game-area">
+        <iframe src="" frameborder="0" class="game-frame">
+
+        </iframe>
+    </section>
+    <aside class="game-leaderboard">
+        <div class="leaderboard-title"></div>
+        <div class="leaderboard-item">
+            <div class="player-rank"></div>
+            <div class="player-name"></div>
+            <div class="player-score"></div>
+        </div>
+        <div class="leaderboard-empty">目前尚無分數紀錄</div>
+    </aside>
+</div>
---
+++ front/games.php
@@ -0,0 +1,10 @@
+<div id="games">
+    <section class="game-list">
+        <div class="game-item">
+            <img src="" alt="" class="game-cover">
+            <div class="game-title"></div>
+            <div class="game-description"></div>
+            <a href="" class="play-game-link"></a>
+        </div>
+    </section>
+</div>
