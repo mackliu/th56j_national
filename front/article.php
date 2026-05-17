296cd58c42c26e1103bdcf8f931fdeedf3dd32ad
mackliu <macklun@gmail.com>
2026-05-17 14:25:04 +0800

    完成文章頁面的顯示

--- front/article.php
+++ front/article.php
@@ -1,10 +1,14 @@
+<?php  include_once '../api/db.php';
+
+$article=$pdo->query("SELECT * FROM `articles` WHERE `id`='{$_GET['id']}'")->fetch();
+?>
+
 <div id="article">
     <header class="article-header">
-        <h1 class="article-title text-center">文章標題</h1>
-        <time datetime="" class="article-date d-block w-100 text-right">發文時間:<?=date("Y-m-d H:i:s");?></time>
+        <h3 class="article-title text-center"><?=$article['title'];?></h3>
+        <time datetime="" class="article-date d-block w-100 text-right">發文日期：<?=date("Y-m-d",strtotime($article['created_at']));?></time>
     </header>
-    <section class="article-body">
-        文章內容
-        
+    <section class="article-body col-md-10 m-auto" >
+       <?php echo nl2br($article['content']);?>
     </section>
 </div>
