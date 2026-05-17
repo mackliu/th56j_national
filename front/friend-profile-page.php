b5c09d9941c125f0aad4a3d3bb7ca0d3fb8de2ca
mackliu <macklun@gmail.com>
2026-05-17 15:59:34 +0800

    完成查看好友頁面及文章功能

--- front/friend-profile-page.php
+++ front/friend-profile-page.php
@@ -1,25 +1,40 @@
+<?php include_once "../api/db.php";
+$friend=$pdo->query("SELECT * FROM `users` WHERE `id`='{$_GET['id']}'")->fetch();
+$userHeader=(!empty($friend['header']))?"./img/{$friend['header']}":"./img/default_header.jpg";
+?>
+
+
+
 <div id="profile-page">
     <section class="profile-header w-100 p-3 border rounded mb-2 text-center">
-        <img src="./img/user_05.jpg" class="profile-avater" style='width:128px'>
-        <div class="profile-username">username</div>
-        <div class="profile-bio m-auto col-md-8">簡介</div>
+        <img src="<?=$userHeader;?>" class="profile-avater" style='width:128px'>
+        <div class="profile-username"><?=$friend['username'];?></div>
+        <div class="profile-bio m-auto col-md-8 form-group border rounded bg-info text-white p-2">
+        <span class="show-bio"><?=($friend['bio']!=='')?$friend['bio']:'尚未填寫自我介紹';?></span>
+        </div>
     </section>
 
 <div class="profile-content">
     <section class="articles my-2 border rounded p-3">
-        <?php for($i=0;$i<5;$i++):?>
+        <?php 
+            $articles=$pdo->query("SELECT * FROM `articles` WHERE `user_id`='{$friend['id']}';")->fetchAll();
+            if(count($articles)>0):
+            foreach($articles as $article):;?>
+
         <article class="article-item my-2">
             <div class='d-flex justify-content-between w-100'>
-                <div class="article-title">文章標題</div>
-                <time datetime="" class="article-date text-sm"><?= date("Y-m-d H:i:s"); ?></time>
+                <div class="article-title bolder"><?=$article['title'];?></div>
+                <time datetime="" class="article-date text-sm"><?= date("Y-m-d",strtotime($article['created_at'])); ?></time>
             </div>
-            <div class='d-flex w-100'>
-                <div class="article-excerpt">摘要...</div>
-                <a href="javascript:loadpage('')" class="article-readmore">閱讀更多</a>
+            <div class='d-flex w-100 pl-3'>
+                <div class="article-excerpt"><?=mb_substr($article['content'],0,30);?>...</div>
+                <a href="javascript:loadpage('./front/article.php?id=<?=$article['id'];?>')" class="article-readmore">閱讀更多</a>
             </div>
         </article>
-        <?php endfor;?>
-        <div class="empty-article-message">目前尚無文章</div>
+        <?php 
+            endforeach;  else:;?>
+            <div class="empty-article-message">目前尚無文章</div>
+             <?php endif;?>
     </section>    
 </div>
 <div class="profile-friend-actions">
