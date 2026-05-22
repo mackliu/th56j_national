4c8cbff6df5db72fd600c75f831153e9d37b84ab
mackliu <macklun@gmail.com>
2026-05-23 00:31:13 +0800

    修改遊戲頁面

--- front/games.php
+++ front/games.php
@@ -2,9 +2,9 @@
     <section class="game-list">
         <div class="game-item">
             <img src="" alt="" class="game-cover">
-            <div class="game-title"></div>
-            <div class="game-description"></div>
-            <a href="" class="play-game-link"></a>
+            <div class="game-title">遊戲標題</div>
+            <div class="game-description">遊戲描述</div>
+            <a href="" class="play-game-link">開始遊戲</a>
         </div>
     </section>
 </div>
