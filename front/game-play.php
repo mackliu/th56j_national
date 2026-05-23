3b08108b95858d3c1e421e33d8181366a6b0fcd8
mackliu <macklun@gmail.com>
2026-05-23 15:48:03 +0800

    建立遊戲功能頁面及遊戲載入iframe功能

--- front/game-play.php
+++ front/game-play.php
@@ -1,17 +1,26 @@
+<?php
+include_once "../api/db.php";
+$game=$pdo->query("select * from `games` where `id`='{$_GET['id']}'")->fetch();
+$game_setting=json_decode(file_get_contents("../games/{$game['id']}/game.json"));
+              
+?>
 <div id="game-play">
-    <div class="current-game-title"></div>
+    <h2 class="current-game-title text-center"><?=$game['title'];?></h2>
     <section class="game-area">
-        <iframe src="" frameborder="0" class="game-frame">
+        <iframe src="<?=$game_setting->entry->url;?>" frameborder="0" class="game-frame d-block m-auto w-75 " style="height:400px;" >
 
         </iframe>
     </section>
     <aside class="game-leaderboard">
-        <div class="leaderboard-title"></div>
+        <h2 class="leaderboard-title text-center"><?=$game['title'];?>風雲排行榜</h2>
+        
         <div class="leaderboard-item">
             <div class="player-rank"></div>
             <div class="player-name"></div>
             <div class="player-score"></div>
         </div>
-        <div class="leaderboard-empty">目前尚無分數紀錄</div>
+        
+        <div class="leaderboard-empty text-center">目前尚無分數紀錄</div>
     </aside>
 </div>
+
--- front/games.php
+++ front/games.php
@@ -1,10 +1,17 @@
+<?php include_once "../api/db.php";?>
 <div id="games">
-    <section class="game-list">
-        <div class="game-item">
-            <img src="" alt="" class="game-cover">
-            <div class="game-title"></div>
-            <div class="game-description"></div>
-            <a href="" class="play-game-link"></a>
+
+    <section class="game-list d-flex flex-wrap p-3 ">
+        <?php 
+        $games=$pdo->query("select * from `games` ")->fetchAll();
+        foreach($games as $game):?>
+        <div class="game-item my-3 col-4 border rounded py-2">
+            <img src="<?=$game['cover'];?>" alt="" class="game-cover w-100 mb-2">
+            <div class="game-title"><?=$game['title'];?></div>
+            <div class="game-description mb-2"><?=$game['description'];?></div>
+            <a href="javascript:loadpage('./front/game-play.php?id=<?=$game['id'];?>')" class="play-game-link btn btn-lg btn-success d-block m-auto">開始遊戲</a>
+            
         </div>
+        <?php endforeach;?>
     </section>
 </div>
