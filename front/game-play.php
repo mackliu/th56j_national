58e4d1f5621cdf6fa6d36ca75b0a280639ca4d20
mackliu <macklun@gmail.com>
2026-05-31 14:30:10 +0800

    遊戲內容,改為全用ajax方式載入遊戲和排行榜

--- front/game-play.php
+++ front/game-play.php
@@ -1,13 +1,13 @@
 <?php
 include_once "../api/db.php";
 $game=$pdo->query("select * from `games` where `id`='{$_GET['id']}'")->fetch();
-$game_setting=json_decode(file_get_contents("../games/{$game['id']}/game.json"));
+//$game_setting=json_decode(file_get_contents("../games/{$game['id']}/game.json"));
               
 ?>
 <div id="game-play">
     <h2 class="current-game-title text-center"><?=$game['title'];?></h2>
     <section class="game-area">
-        <iframe src="<?=$game_setting->entry->url;?>" frameborder="0" class="game-frame d-block m-auto w-75 " style="height:600px;" >
+        <iframe src="" frameborder="0" class="game-frame d-block m-auto w-75 " style="height:600px;" >
 
         </iframe>
     </section>
@@ -20,12 +20,20 @@ $game_setting=json_decode(file_get_contents("../games/{$game['id']}/game.json"))
 </div>
 
 <script>
-    $.get("<?= $game_setting->score->pullUrl ?>",(ranks)=>{
+
+    $.get("games/<?=$_GET['id'];?>/game.json",(game_setting)=>{
+        console.log(game_setting.score.pullUrl)
+        $("iframe").attr("src",`${game_setting.entry.url}`)
+        getLeaderBoard(game_setting.score.pullUrl)
+    })
+
+function getLeaderBoard(url){
+    $.get(url,(ranks)=>{
         if(ranks.length>0){
                let cols=`<div class="leaderboard-item list-group-item d-flex text-center">
                             <div class="player-rank col-2">排名</div>
-                            <div class="player-name col-6"><?= $game_setting->score->columns[0]; ?></div>
-                            <div class="player-score col-2"><?= $game_setting->score->columns[1];?></div>
+                            <div class="player-name col-6">名稱</div>
+                            <div class="player-score col-2">分數</div>
                         </div>`
                         $("#leaderboard").append(cols)
             ranks.forEach((item,idx)=>{
@@ -40,4 +48,5 @@ $game_setting=json_decode(file_get_contents("../games/{$game['id']}/game.json"))
             $("#leaderboard").append("<div class='leaderboard-empty text-center'>目前尚無分數紀錄</div>")
         }
     })
+}
 </script>
